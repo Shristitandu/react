0d85f8c31bd1402d7b415e8754eaa060e40eae70
Shristitandu <shristi.tandu11@gmail.com>
2024-11-04 12:15:46 +0545

    Delete react/todo-cra/deploy.sh

--- react/todo-cra/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# SSH into your server using plink and deploy the Docker Compose stack
-plink -i "C:\Users\Shristi\Downloads\namecheap_test (1).ppk" testUser@162.0.216.97 << 'EOF'
-  cd /path/to/your/project
-  docker-compose pull
-  docker-compose up -d
-EOF
